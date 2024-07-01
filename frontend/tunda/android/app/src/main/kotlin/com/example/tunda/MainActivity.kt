ad973ec60796d800ea468eff1b42a9a26190f6a1
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-07-01 22:12:46 +0300

    create: frontend folder and files including the main dart file with few UI implementation

---
+++ frontend/tunda/android/app/src/main/kotlin/com/example/tunda/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tunda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ frontend/tunda/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'database_helper.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Offline Mobile App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final dbHelper = DatabaseHelper.instance;
+  final String table = 'banana';
+
+  void _insert() async {
+    Map<String, dynamic> row = {
+      'stocking_date': '2024-06-20',
+      'status': 'ripe',
+      'expected_expiry_date': '2024-06-27'
+    };
+    final id = await dbHelper.insert(table, row);
+    print('inserted row id: $id');
+  }
+
+  void _query() async {
+    final allRows = await dbHelper.queryAllRows(table);
+    print('query all rows:');
+    allRows.forEach(print);
+  }
+
+  void _update() async {
+    Map<String, dynamic> row = {
+      'id': 1,
+      'stocking_date': '2024-06-22',
+      'status': 'unripe',
+      'expected_expiry_date': '2024-07-02'
+    };
+    final rowsAffected = await dbHelper.update(table, row);
+    print('updated $rowsAffected row(s)');
+  }
+
+  void _delete() async {
+    final id = await dbHelper.delete(table, 1);
+    print('deleted $id row(s)');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Offline Mobile App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: _insert,
+              child: Text('Insert'),
+            ),
+            ElevatedButton(
+              onPressed: _query,
+              child: Text('Query'),
+            ),
+            ElevatedButton(
+              onPressed: _update,
+              child: Text('Update'),
+            ),
+            ElevatedButton(
+              onPressed: _delete,
+              child: Text('Delete'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
