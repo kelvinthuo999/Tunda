183066e16bc47e2a77c638318b0ccdfe5b52694e
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-07-02 08:22:27 +0300

    add comments to the server file

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+// import all the necessary modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const {
@@ -6,12 +7,12 @@ const {
   readData,
   deleteTable
 } = require('./database');
-
+// start the app and open the port it will be running
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-
+//endpoint to create a table
 app.post('/createTable', async (req, res) => {
   const { tableName } = req.body;
   try {
@@ -21,7 +22,7 @@ app.post('/createTable', async (req, res) => {
     res.status(500).send(`Error creating table ${tableName}: ${error.message}`);
   }
 });
-
+//endpoint to insert data into a table
 app.post('/insertData', async (req, res) => {
   const { tableName, data } = req.body;
   try {
@@ -31,7 +32,7 @@ app.post('/insertData', async (req, res) => {
     res.status(500).send(`Error inserting data into table ${tableName}: ${error.message}`);
   }
 });
-
+//endpoint to read data from a table
 app.get('/readData', async (req, res) => {
   const { tableName } = req.query;
   try {
@@ -41,7 +42,7 @@ app.get('/readData', async (req, res) => {
     res.status(500).send(`Error reading data from table ${tableName}: ${error.message}`);
   }
 });
-
+//endpoint to delete a table
 app.delete('/deleteTable', async (req, res) => {
   const { tableName } = req.body;
   try {
